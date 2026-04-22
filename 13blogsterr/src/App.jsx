70b08cce06dc4b508db52190f557d8a8f27697d5
sharma-mayankkk <br10216030515@gmail.com>
2026-04-22 21:56:25 +0530

    feat: implement user authentication and loading state in App component

--- 13blogsterr/src/App.jsx
+++ 13blogsterr/src/App.jsx
@@ -1,13 +1,35 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from "react"
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logOut } from './store/authSlice'
+import {Header,Footer} from "./components"
 
 function App() {
-  console.log()
-  return (
-    <>
-      <h1>Appwrite project</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logOut())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className="min-h-screen flex-wrap content-between bg-gray-500">
+      <div className="w-full items-center">
+        <Header/>
+          <main>
+             todo: Outlet
+          </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : (null)
 }
 
 export default App
